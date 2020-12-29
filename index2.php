c009a70412948ed1837e1705eb5b90c18d4e91e6
parvaeti <parvathi742002@gmail.com>
2020-12-29 22:38:48 +0530

    augusto completed

--- index2.php
+++ index2.php
@@ -1,57 +1,68 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <link rel="stylesheet" href="pica\style.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>☸ ＡＵＧＵＳＴＲＯ - 𝓢𝓘𝓖𝓝 𝓤𝓟</title>
 </head>
-<body>
+<body class="mybackground" >
     <div class="container">
         <div class="row">
             <div class="col "></div>
             <div class="col col-12 col-sm-12 col-md-4 col-lg-4 col-xl-4 ">
-                <table class="table">
+                <table class="table" >
                     
                            <h1>  ⫷  𝘚𝘐𝘎𝘕 𝘜𝘗  ⫸ </h1>
                         <tr>
                         <td>ᖴIᖇᔕT ᑎᗩᗰE :</td>
-                        <td><input type="text" class="form-control"></td>
+                        <td><input id="fname" type="text" class="form-control"></td>
                     </tr>
                     <tr>
                         <td>   ᒪᗩᔕT ᑎᗩᗰE :
                           </td>
-                        <td><input type="text" class="form-control"></td>
+                        <td><input id="lname" type="text" class="form-control"></td>
                     </tr>
                     <tr>
                         <td>  EᗰᗩIᒪ :
                         </td>
-                        <td><input type="text" class="form-control"></td>
+                        <td><input id="email" type="text" class="form-control"></td>
                     </tr>
-                    <tr>
-                        <td> ᗰOᗷIᒪE ᑎᑌᗰᗷEᖇ :
-                        </td>
-                        <td><input type="text" class="form-control"> </td>
-                    </tr>
+                    
                     <tr>
                         <td>ᑭᗩᔕᔕᗯOᖇᗪ :
                            </td>
-                        <td><input type="text" class="form-control"></td>
+                        <td><input id="pass" type="text" class="form-control"></td>
                     </tr>
                     <tr>
                         <td> ᑕOᑎᖴIᖇᗰ ᑭᗩᔕᔕᗯOᖇᗪ :</td>
-                        <td><input type="text" class="form-control"></td>
+                        <td><input id="cpass" type="text" class="form-control"></td>
                     </tr>
                     <tr>
                         <td><a class="btn btn-secondary" href="index.php" role="button">🌷 ＢＡＣＫ  
                         </a></td>
-                        <td><button class="btn btn-dark">ＣＲＥＡＴＥ</button></td>
+                        <td><button onclick="clickonthebutton()" class="btn btn-dark">ＣＲＥＡＴＥ</button></td>
                     </tr>
                 </table>
             </div>
             <div class="col"></div>
         </div>
     </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>  
+    <script>
+    function clickonthebutton(){
+        var getᖴIᖇᔕTᑎᗩᗰE=document.getElementById("fname").value
+        var getᒪᗩᔕTᑎᗩᗰE=document.getElementById("lname").value
+        var getEᗰᗩIᒪ=document.getElementById("email").value
+        var getᑭᗩᔕᔕᗯOᖇᗪ=document.getElementById("pass").value
+        var getᑕOᑎᖴIᖇᗰᑭᗩᔕᔕᗯOᖇᗪ=document.getElementById("cpass").value
+        
+        console.log(getᖴIᖇᔕTᑎᗩᗰE)
+        console.log( getᒪᗩᔕTᑎᗩᗰE)
+        console.log(getEᗰᗩIᒪ)
+        console.log(getᑭᗩᔕᔕᗯOᖇᗪ)
+        console.log(getᑕOᑎᖴIᖇᗰᑭᗩᔕᔕᗯOᖇᗪ)
+    }
+    </script>
 </body>
 </html>
--- index3.php
+++ index3.php
@@ -1,12 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <link rel="stylesheet" href="pica\style.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>☸ ＡＵＧＵＳＴＲＯ - 𝓢𝓘𝓖𝓝 𝓤𝓟</title>
 </head>
-<body>
+<body class="myback" >
     <div class="container">
        
         <div class="row">
@@ -14,22 +15,22 @@
             <div class="col col-12 col-sm-12 col-md-4 col-lg-4 col-xl-4 ">
                 <table class="table">
                     
-                           <h1>  ⫷  𝓢𝓘𝓖𝓝 𝓘𝓝  ⫸ </h1>
+                           <h1 >  ⫷  𝓢𝓘𝓖𝓝 𝓘𝓝  ⫸ </h1>
                           
                       
                     <tr>
                         <td>EᗰᗩIᒪ :</td>
-                        <td><input type="text" class="form-control"></td>
+                        <td><input id="email" type="text" class="form-control"></td>
                     </tr>
                     <tr>
                         <td>ᑭᗩᔕᔕᗯOᖇᗪ : </td>
-                        <td><input type="text" class="form-control"> </td>
+                        <td><input id="pass" type="text" class="form-control"> </td>
                     </tr>
                    
                     <tr>
                         <td><a class="btn btn-secondary" href="index.php" role="button">🌷 ＢＡＣＫ  
                         </a></td>
-                        <td><button class="btn btn-dark">ＳＩＧＮ ＩＮ</button></td>
+                        <td><button onclick="clickbutton()" class="btn btn-dark">ＳＩＧＮ ＩＮ</button></td>
                     </tr>
                 </table>
             </div>
@@ -37,6 +38,14 @@
         </div>
     </div>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
+   <script>
+       function clickbutton(){
+           var getEᗰᗩIᒪ=document.getElementById("email").value
+           var getᑭᗩᔕᔕᗯOᖇᗪ=document.getElementById("pass").value
+           
+           console.log(getEᗰᗩIᒪ)
+           console.log(getᑭᗩᔕᔕᗯOᖇᗪ)
+       }
+   </script>
 </body>
 </html>
